f959f7adb3b48ee7198be97a1ca4349f5c8008c7
BM-ACADEMY <teamkamar@gmail.com>
2026-05-11 17:12:47 +0530

    new

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.bm27tmajcdg"
+    "revision": "0.tq10bcj3cd8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
