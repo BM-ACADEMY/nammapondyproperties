c730abaf4c686dae913975cd0a4237928562df87
BM-ACADEMY <teamkamar@gmail.com>
2026-05-11 10:59:02 +0530

    marketing plans

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.f5mkg8a8mus"
+    "revision": "0.bm27tmajcdg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
