50f184f477154ffaf9991d4e5112b8de5b599248
BM-ACADEMY <teamkamarweb@gmail.com>
2026-08-12 16:29:42 +0530

    feat: initialize service worker with Workbox precaching and navigation routing

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.f486o6l5p3"
+    "revision": "0.88gflbrksf"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
